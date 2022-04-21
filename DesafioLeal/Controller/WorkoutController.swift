610ba4ae3c48a52e5b66a480ddf01f72d4de88a4
Andre Linces <andrelinces@yahoo.com.br>
2022-04-21 10:38:40 -0300

    Created function: (update), for the workouts to of the user. - organization of the code.

--- DesafioLeal/Controller/WorkoutController.swift
+++ DesafioLeal/Controller/WorkoutController.swift
@@ -11,6 +11,7 @@ import FirebaseFirestore
 import FirebaseAuth
 import SwiftUI
 
+
 class WorkoutController: UIViewController, ModelExercisesPosterCallBack, ModelWorkoutPosterCellCallBack, ModelWorkoutCellCallBack, ModelTitleMyWorkoutCellCallBack {
     
     func actionReturn() {
@@ -24,27 +25,17 @@ class WorkoutController: UIViewController, ModelExercisesPosterCallBack, ModelWo
         
         if indexPath.row > -1  {
             
-            
-            // [Alert for to user, account created successfully]
-            let alert = UIAlertController(title:  "Menu Workout", message: "what do you want to do ? ?", preferredStyle: .alert)
-           
-            let cancelAlert = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            alertEditWorkout()
         
-            let confirmAction = UIAlertAction(title: "To edit", style: .default) { alertAction in
-            //testing...
-            print("confirmAction")
+            self.tableViewWorkout.reloadData()
             
-                self.performSegue(withIdentifier: "segueMyWorkout" , sender: nil)
         }
-            alert.addAction(cancelAlert)
-            alert.addAction(confirmAction)
-            
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
+        
+    }//[end indexPaath]
  
     var db = Firestore.firestore()
     var handle: AuthStateDidChangeListenerHandle?
+    var auth = Auth.auth()
     
     //Variables inicialization
     var imagePosterWorkout : String = ""
@@ -57,16 +48,123 @@ class WorkoutController: UIViewController, ModelExercisesPosterCallBack, ModelWo
         self.imagePosterWorkout = imagePosterWorkout
         self.titleWorkout = titleWorkout
         self.descriptionWorkout = descriptionWorkout
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         //diplayUserWorkout ()
-     
         
     }//[end didAppear]
+    
+    func alertEditWorkout () {
+        
+            let alertEdit = UIAlertController(title: "To edit!", message: "Do you want to edit your workout?", preferredStyle: .alert)
+            
+            let confirmEdit = UIAlertAction(title: "To edit", style: .default, handler: {(_ action: UIAlertAction) -> Void in
+                
+                self.performSegue(withIdentifier: "segueMyWorkout", sender: nil)
+                })
+            
+            let confirmUpdate = UIAlertAction(title: "Update name", style: .default, handler: {(_ action: UIAlertAction) -> Void in
+                
+                // Create Update alert controller
+                    let alertController = UIAlertController(title: "Update Name", message: "Type name for to workout!", preferredStyle: .alert)
 
+                    // add textfield at index 0
+                    alertController.addTextField(configurationHandler: {(_ textFieldName: UITextField) -> Void in
+                        textFieldName.placeholder = "Name"
+                        self.present(alertController, animated: true, completion: nil)
+       
+                                let confirmAction = UIAlertAction(title: "OK", style: .default, handler: {(_ action: UIAlertAction) -> Void in
+                
+                                    self.myWorkout = alertController.textFields?[0].text ?? "default test field"
+                                    
+                                    var handle: AuthStateDidChangeListenerHandle?
+                                    
+                                    if alertController.textFields?[0].text == "" {
+
+                                        let alert = UIAlertController(title: "Empty field", message: "Type name or categorie for to workout!", preferredStyle: .alert)
+
+                                        let cancel = UIAlertAction(title: "Cancel", style: .default, handler: {(_ action: UIAlertAction) -> Void in
+                                            print("Canelled")
+                                            
+                                        })
+                                        alert.addAction(cancel)
+                                        self.present(alert, animated: true, completion: nil)
+                
+                                    }else{
+                                        
+                                    let userId = self.auth.currentUser?.uid
+                                    var refNewName = self.db.collection("users").document(userId ?? "default").collection("workout").document(userId ?? "default")
+                                    
+                                    // Set the reference for to updating document.
+                                        refNewName.updateData ([
+                                        
+                                        "name": alertController.textFields?[0].text
+                                        
+                                    ])
+                                        { err in
+                                        if let err = err {
+                                            print("Error updating document: \(err)")
+                                        } else {
+                                            self.tableViewWorkout.reloadData()
+                                            print("Document successfully updated")
+                                            
+                                            let alertUpdate = UIAlertController(title: "Update Document Successfully!", message: "Updated Document  successfully, click a edit, for to display all information from workout and add exercises.", preferredStyle: .alert)
+
+                                            let okUpdate = UIAlertAction(title: "Ok", style: .default, handler: {(_ action: UIAlertAction) -> Void in
+                                                print("okUpadte")
+                                                
+                                                    self.tableViewWorkout.reloadData()
+                                                    print("async, tableview")
+                                                   
+                                                self.presentedViewController?.reloadInputViews()
+                                            })
+                                            alertUpdate.addAction(okUpdate)
+                                            self.present(alertUpdate, animated: true, completion: nil)
+                                            
+                                        }
+                                        
+                                    }
+                                   
+                                    }//[end else]
+                                    alertEdit.addAction(confirmEdit)
+                                    
+                                })
+ 
+                                alertController.addAction(confirmAction)
+    
+                    })
+               
+                })
+            self.tableViewWorkout.reloadData()
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: {(_ action: UIAlertAction) -> Void in
+              
+                })
+           
+                alertEdit.addAction(confirmEdit)
+                alertEdit.addAction(confirmUpdate)
+                alertEdit.addAction(cancel)
+           
+                self.present(alertEdit, animated: true, completion: nil)
+            
+            // Create Update alert controller
+                let alertController = UIAlertController(title: "Update Name", message: "Type name for to workout!", preferredStyle: .alert)
+
+                // Alert action cancel
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {(_ action: UIAlertAction) -> Void in
+                    print("Canelled")
+                
+                })
+                alertController.addAction(cancelAction)
+            
+                // Present alert controller
+                present(alertController, animated: true, completion: nil)
+                self.tableViewWorkout.reloadData()
+        
+        
+        
+    }//[End alertEditWorkout]
     
     func diplayUserWorkout () {
        
@@ -88,7 +186,7 @@ class WorkoutController: UIViewController, ModelExercisesPosterCallBack, ModelWo
                     
                     print("userID \(String(describing: (worRef)))")
                     
-                    worRef.getDocuments() { (querySnapshot, err) in
+                worRef.getDocuments { (querySnapshot, err) in
                     if let err = err {
                         print("Error getting documents: \(err)")
                         
@@ -126,8 +224,7 @@ class WorkoutController: UIViewController, ModelExercisesPosterCallBack, ModelWo
                     }
                         
                     }//[end get workout]
-                    
-                    
+                      
                 }
                 
             }
@@ -148,7 +245,6 @@ class WorkoutController: UIViewController, ModelExercisesPosterCallBack, ModelWo
         self.dismiss(animated: true, completion: nil)
     }
     
-   
     
     func setupTableView () {
         
--- DesafioLeal/Global Helpers/Global Extension/Extension.swift
+++ DesafioLeal/Global Helpers/Global Extension/Extension.swift
@@ -49,6 +49,72 @@ extension UIView {
             guard let url = URL(string: link) else { return }
             downloaded(from: url, contentMode: mode)
         }
+        
+        // Create alert controller
+//            let alertController = UIAlertController(title: "Update Name", message: "Type name for to workout!", preferredStyle: .alert)
+//
+//            // add textfield at index 0
+//            alertController.addTextField(configurationHandler: {(_ textFieldName: UITextField) -> Void in
+//                textFieldName.placeholder = "Name"
+//                
+//            })
+//
+//            // add textfield at index 1
+//            alertController.addTextField(configurationHandler: {(_ textFieldCategorie: UITextField) -> Void in
+//                textFieldCategorie.placeholder = "Categorie"
+//
+//            })
+              
+            // Alert action confirm
+//            let confirmAction = UIAlertAction(title: "OK", style: .default, handler: {(_ action: UIAlertAction) -> Void in
+//                print("name: \(String(describing: alertController.textFields?[0].text))")
+//                print("email: \(String(describing: alertController.textFields?[1].text))")
+//                self.myWorkout = alertController.textFields?[0].text ?? "default test field"
+//                
+//                var handle: AuthStateDidChangeListenerHandle?
+//                
+//                if alertController.textFields?[0].text == "" || alertController.textFields?[1].text == "" {
+//
+//                    let alert = UIAlertController(title: "Empty field", message: "Type name or categorie for to workout!", preferredStyle: .alert)
+//
+//                    let cancel = UIAlertAction(title: "Cancel", style: .default, handler: {(_ action: UIAlertAction) -> Void in
+//                        print("Canelled")
+//                        
+//                    })
+//                    alert.addAction(cancel)
+//                    self.present(alert, animated: true, completion: nil)
+////                        })
+//                }else{
+//                let userId = self.auth.currentUser?.uid
+//                var refNewName = self.db.collection("users").document(userId ?? "default").collection("workout").document(userId ?? "default")
+//                
+//                // Set the reference for to updating document.
+//                refNewName.updateData([
+//                    
+//                    "name": alertController.textFields?[0].text
+//                    
+//                ]) { err in
+//                    if let err = err {
+//                        print("Error updating document: \(err)")
+//                    } else {
+//                        print("Document successfully updated")
+//                        self.tableViewWorkout.reloadData()
+//                    }
+//                }
+//                }//[end else]
+//            })
+//    
+//            alertController.addAction(confirmAction)
+//        
+//            // Alert action cancel
+//            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {(_ action: UIAlertAction) -> Void in
+//                print("Canelled")
+//            
+//            })
+//            alertController.addAction(cancelAction)
+//
+//            // Present alert controller
+//            present(alertController, animated: true, completion: nil)
     }
 
 
